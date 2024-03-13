507978b16a6f804151986dd69d282a1f6cf80ab9
Maksym Bondarenko <mabondaren@gmail.com>
2024-03-12 21:43:13 -0700

    added more unit tests

--- trie_test.cc
+++ trie_test.cc
@@ -28,10 +28,108 @@ TEST(TrieTest, RemoveBasicTest) {
 TEST(TrieTest, FindBasicTest) {
     TrieNode *root = new TrieNode();
     add(root, "abcd");
-    
+
     EXPECT_TRUE(find(root, "abcd"));
     EXPECT_TRUE(find(root, "abc"));
     EXPECT_TRUE(find(root, "ab"));
     EXPECT_TRUE(find(root, "a"));
     EXPECT_FALSE(find(root, "bcd"));
+}
+
+TEST(TrieTest, RemoveTest) {
+    TrieNode *root = new TrieNode();
+    add(root, "abcd");
+    add(root, "abc");
+    add(root, "ab");
+    add(root, "a");
+    EXPECT_TRUE(find(root, "abcd"));
+
+    remove(root, "abcd");
+    EXPECT_FALSE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+
+    remove(root, "abc");
+    EXPECT_FALSE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+
+    remove(root, "ab");
+    EXPECT_FALSE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_FALSE(find(root, "a"));
+}
+
+TEST(TrieTest, RemoveTest2) {
+    TrieNode *root = new TrieNode();
+    add(root, "a");
+    add(root, "abcd");
+    add(root, "ab");
+    add(root, "abc");
+    
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "ab");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "abc");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "abcd");
+    EXPECT_FALSE(find(root, "a"));
+    EXPECT_FALSE(find(root, "ab"));
+    EXPECT_FALSE(find(root, "abc"));
+    EXPECT_FALSE(find(root, "abcd"));
+}
+
+TEST(TrieTest, RemoveTest3) {
+    TrieNode *root = new TrieNode();
+    add(root, "a");
+    add(root, "abcd");
+    add(root, "ab");
+    add(root, "abc");
+    
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_TRUE(find(root, "abcd"));
+    EXPECT_TRUE(find(root, "abc"));
+    EXPECT_TRUE(find(root, "ab"));
+    EXPECT_TRUE(find(root, "a"));
+
+    remove(root, "a");
+    EXPECT_FALSE(find(root, "a"));
+    EXPECT_FALSE(find(root, "ab"));
+    EXPECT_FALSE(find(root, "abc"));
+    EXPECT_FALSE(find(root, "abcd"));
 }
